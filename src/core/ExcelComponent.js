810c7325a896c7c5945d04cb6cae7fe89f028a76
Anton Pakhomov <antonn.pakhomovv@gmail.com>
2025-12-24 14:37:05 +0400

    Routing implemented

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,57 +1,46 @@
-import {DomListener} from '@core/DomListener'
+import { DomListener } from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
-  constructor($root, options = {}) {
-    super($root, options.listeners)
-    this.name = options.name || ''
-    this.emitter = options.emitter
-    this.subscribe = options.subscribe || []
-    this.store = options.store
-    this.unsubscribers = []
+	constructor($root, options = {}) {
+		super($root, options.listeners);
+		this.name = options.name || '';
+		this.emitter = options.emitter;
+		this.subscribe = options.subscribe || [];
+		this.store = options.store;
+		this.unsubscribers = [];
 
-    this.prepare()
-  }
+		this.prepare();
+	}
 
-  // Настраивааем наш компонент до init
-  prepare() {}
+	prepare() {}
 
-  // Возвращает шаблон компонента
-  toHTML() {
-    return ''
-  }
+	toHTML() {
+		return '';
+	}
 
-  // Уведомляем слушателей про событие event
-  $emit(event, ...args) {
-    this.emitter.emit(event, ...args)
-  }
+	$emit(event, ...args) {
+		this.emitter.emit(event, ...args);
+	}
 
-  // Подписываемся на событие event
-  $on(event, fn) {
-    const unsub = this.emitter.subscribe(event, fn)
-    this.unsubscribers.push(unsub)
-  }
+	$on(event, fn) {
+		const unsub = this.emitter.subscribe(event, fn);
+		this.unsubscribers.push(unsub);
+	}
 
-  $dispatch(action) {
-    this.store.dispatch(action)
-  }
+	$dispatch(action) {
+		this.store.dispatch(action);
+	}
 
-  // Сюда приходят только изменения по тем полям, на которые мы подписались
-  storeChanged() {}
+	storeChanged() {}
 
-  isWatching(key) {
-    return this.subscribe.includes(key)
-  }
-
-  // Инициализируем компонент
-  // Добавляем DOM слушателей
-  init() {
-    this.initDOMListeners()
-  }
-
-  // Удаляем компонент
-  // Чистим слушатели
-  destroy() {
-    this.removeDOMListeners()
-    this.unsubscribers.forEach(unsub => unsub())
-  }
+	isWatching(key) {
+		return this.subscribe.includes(key);
+	}
+	init() {
+		this.initDOMListeners();
+	}
+	destroy() {
+		this.removeDOMListeners();
+		this.unsubscribers.forEach(unsub => unsub());
+	}
 }
